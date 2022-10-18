928b850c88157181116407c4a2baf8f96d798c14
AnahVi <ana.vitoria.barbosa.mariano@gmail.com>
2022-10-18 20:05:40 -0300

    Ajuste rotas adição de modulos

---
+++ agendamento/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name="home"),
+    #path('form/', views.form, name="form"),
+    #path('create/', views.create, name="create"),
+    #path('view/<int:pk>/', views.view, name="view"),
+    #path('edit/<int:pk>/', views.edit, name="edit"),
+    #path('update/<int:pk>/', views.update, name="update"),
+    #path('delete/<int:pk>/', views.delete, name="delete"),
+]
