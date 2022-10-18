32ce67ddd6436171dd67d8596b1086a5d467b898
AnahVi <ana.vitoria.barbosa.mariano@gmail.com>
2022-10-18 20:20:50 -0300

    Adição das funções da view

--- horario/views.py
+++ horario/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    data = {
+        'nomefuncao': 'Horário',
+    }
+    return render(request, 'horario.html', data)
--- procedimento/views.py
+++ procedimento/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    data = {
+        'nomefuncao': 'Procedimento',
+    }
+    return render(request, 'procedimento.html', data)
